00b48dbe4b00e5a3e1d689416e1c8c22f4b9cb5f
Akshay Saini <anonyxhappie@gmail.com>
2017-10-30 01:10:10 +0530

    auth added

--- feedback/js/main.js
+++ feedback/js/main.js
@@ -1,36 +1,72 @@
 // Initialize Firebase
 var config = {
-apiKey: "AIzaSyB7OBVdghZHJ0-VKlC9wq1EWAFxute-IGM",
-authDomain: "time-tracking-b4b81.firebaseapp.com",
-databaseURL: "https://time-tracking-b4b81.firebaseio.com",
-projectId: "time-tracking-b4b81",
-storageBucket: "time-tracking-b4b81.appspot.com",
-messagingSenderId: "251184367762"
+    apiKey: "AIzaSyB7OBVdghZHJ0-VKlC9wq1EWAFxute-IGM",
+    authDomain: "time-tracking-b4b81.firebaseapp.com",
+    databaseURL: "https://time-tracking-b4b81.firebaseio.com",
+    projectId: "time-tracking-b4b81",
+    storageBucket: "time-tracking-b4b81.appspot.com",
+    messagingSenderId: "251184367762"
 };
 
-firebase.initializeApp(config);
+var app = firebase.initializeApp(config),
+    database = app.database(),
+    auth = app.auth();
 
-var dataRef = firebase.database().ref('feedback');
+/**
+* For Authorization
+*/
+function authCheck(){
+    var provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider).then(function(result){
+        //console.log("log:::"+result.user.email);
+        if(result.user.email==='akkilsl522@gmail.com'){
+            document.getElementById('login').style.display = 'none';
+            getFeedbacks();
+        }else{
+            auth.signOut();
+            console.log('Logged Out.');
+            alert('Sorry, You are not authorized.\n\nContact Admin to get access.');
+        }
+    });
+}
 
+/**
+* Get and update feedbacks when authorized
+*/
+function getFeedbacks(){
+    var dataRef = database.ref('feedback');
     dataRef.on('child_added', function(snapshot){
         var newF = snapshot.val();
         createFeedbackElement(newF.name, newF.feedback);
-    });
+});
 // dataRef.on('value', function(data){
 //     data.forEach(function(element) {
 //         var newF = element.val();
 //         createFeedbackElement(newF.name, newF.feedback);    
 //     });
+
+//    /**
+//     * Reload page when newFeedback inserted or deleted
+//     */     
 //     if(data.numChildren() != document.getElementById('feedback').childElementCount){
 //         location.reload();
 //     }
 // });
+}
 
+
+/**
+* Create feedback element 
+* <div>
+*    <h3>{{name}}</h3>
+*    <p>{{feedback}}</p>
+* </div>
+*/
 function createFeedbackElement(name, feedback){        
     var nameH3 = document.createElement('h3');
     var nameTextNode = document.createTextNode(name);
     nameH3.appendChild(nameTextNode);
-    
+
     var feedbackP = document.createElement('p');
     var feedbackTextNode = document.createTextNode(feedback);
     feedbackP.appendChild(feedbackTextNode);
